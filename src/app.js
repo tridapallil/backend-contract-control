76bfb8ca4fc76e33b96160e4d2561dd1e50c8d83
Luiz <tridapallil@gmail.com>
2024-02-12 13:39:00 -0300

    feat: swagger config

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@ const bodyParser = require('body-parser')
 const routes = require('./routes')
 const { sequelize } = require('./model')
 const app = express()
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../docs/swagger.json')
+
 app.use(bodyParser.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 app.use(routes)
