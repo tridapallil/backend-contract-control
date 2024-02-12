dc3810018ae5ed2b5a8ce18c047425ed93bc3d8d
Luiz <tridapallil@gmail.com>
2024-02-12 14:28:56 -0300

    feat: log message

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ init()
 async function init() {
   try {
     app.listen(3001, () => {
-      console.log('Express App Listening on Port 3001')
+      console.log('Express App Listening on http://localhost:3001/')
+      console.log('Swagger UI is running on http://localhost:3001/api-docs/')
     })
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`)
